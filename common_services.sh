f446fb0551b64af3022208c8cb028523cab0151e
gurubhai <mail2syedarmani@gmail.com>
2013-03-14 14:17:54 +0530

    Add sudo before upgrade command.

--- common_services.sh
+++ common_services.sh
@@ -3,9 +3,9 @@
 # Check if openstack configuration file is present, if yes then source it.
 if [[ -f osconfig.conf ]]
 then
-		. osconfig.conf
+	. osconfig.conf
 else
-		 echo "Configuration file not found. Please create osconfig.conf"
+	 echo "Configuration file not found. Please create osconfig.conf"
          exit 1
 fi
 
@@ -16,7 +16,8 @@ configure_cloud_archive(){
 	echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/folsom main" | sudo tee -a /etc/apt/sources.list.d/cloud.list
 	sudo apt-get -y install ubuntu-cloud-keyring
 	echo "Now updating and upgrading...Please have a cup coffee"
-	sudo apt-get -y update && apt-get -y upgrade
+	sudo apt-get -y update 
+	sudo apt-get -y upgrade
 	}
 
 install_basic_packages(){
