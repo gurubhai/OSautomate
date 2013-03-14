a1a1217fc5ba257bef879ec52692a9a65255b743
gurubhai <mail2syedarmani@gmail.com>
2013-03-14 15:03:05 +0530

    Add script for installing rabbitmq

---
+++ install_rabbit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if openstack configuration file is present, if yes then source it.
+if [[ -f osconfig.conf ]]
+then
+	. osconfig.conf
+else
+	 echo "Configuration file not found. Please create osconfig.conf"
+     exit 1
+fi
+
+install_rabbit()
+{
+
+	sudo apt-get install rabbitmq-server
+	
+}
+
+#Main
+install_rabbit
